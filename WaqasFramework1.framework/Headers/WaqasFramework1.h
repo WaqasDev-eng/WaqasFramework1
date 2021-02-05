46195a32546a5de678ccef9e918c8e1e9da01df1
Waqas_Dev <waqas.khan@nextgeni.net>
2021-02-06 03:05:39 +0500

    framework added

---
+++ WaqasFramework1.framework/Headers/WaqasFramework1.h
@@ -0,0 +1,18 @@
+//
+//  WaqasFramework1.h
+//  WaqasFramework1
+//
+//  Created by Waqas Khan on 06/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaqasFramework1.
+FOUNDATION_EXPORT double WaqasFramework1VersionNumber;
+
+//! Project version string for WaqasFramework1.
+FOUNDATION_EXPORT const unsigned char WaqasFramework1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaqasFramework1/PublicHeader.h>
+
+
